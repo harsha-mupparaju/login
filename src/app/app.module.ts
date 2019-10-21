a37b72d803bd3fc5dd037c17c147c2e94b36ce5e
Your Name <youremail@domain.com>
2019-10-21 18:46:59 +0530

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CustomMaterialModule } from '../material.module';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material';
+import {Router} from '@angular/router';
+import {User} from './user';
+import { AuthenticationService } from '../authentication.service';
+import {FormGroup,FormBuilder,Validators, FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private authenticationService:AuthenticationService,
+    private router:Router) { }
+
+  ngOnInit() {
+  }
+  loginUser(user){
+
+    this.authenticationService.loginUser(user).subscribe(data=>{
+      console.log(data);
+      this.router.navigateByUrl('/user-dashboard');
+    },error=>{
+      this.router.navigateByUrl('');
+
+    });
+  }
+}
